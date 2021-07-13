cf4e294429d730547d61833394ad5e22a4bc696b
Zyl990506 <1033578421@qq.com>
2021-07-13 21:15:56 +0800

    asux

--- lodash/zyl990506-lodash.js
+++ lodash/zyl990506-lodash.js
@@ -1,3 +1,9 @@
 var zhangyongle = function() {
-
+    function chunk(array, size) {
+        var result = []
+        for (var i = 0; i < array.lenght; i += size) {
+            result.push(array.slice(i, i + size))
+        }
+        return result
+    }
 }
